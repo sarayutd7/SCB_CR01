dcddb9e454fd67d5236a6e762add861578d0b6b8
sarayut Duangarpai <sarayutd7@gmail.com>
2024-10-31 16:21:57 +0700

    catchup files

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: true,
+    html: true,
+    json: true
+  },
   projectId: "hkh24s",
   e2e: {
     setupNodeEvents(on, config) {
