c6ad6cad5a150506e50498a2e40ff35cbfc6e072
sarayut Duangarpai <sarayutd7@gmail.com>
2024-10-31 13:24:17 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,38 +1,10 @@
 const { defineConfig } = require("cypress");
-const mochawesome = require('cypress-mochawesome-reporter/plugin');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports',   // ที่เก็บรายงาน  
-    reportFilename: 'test-report',
-    overwrite: true,
-    html: true,
-    json: false,
-    timestamp: 'mmddyyyy_HHMMss',
-    charts: true,
-    code: false,
-    autoOpen: true,
-    quiet: false,
-    inline: true,
-    saveJson: true,
-    embeddedScreenshots: true
-  }, 
   projectId: "hkh24s",
   e2e: {
     setupNodeEvents(on, config) {
-      mochawesome(on);  // Add the plugin
-
-      // กำหนดค่า mochawesome reporter โดยไม่ต้อง require Cypress
-      require('cypress-mochawesome-reporter/plugin')(on);
-
-      // Handle after:run without opening the report file
-      on('after:run', (results) => {
-        // ไม่ต้องใช้ xdg-open ใน GitHub Actions
-        // ลบหรือคอมเมนต์คำสั่ง xdg-open ที่อาจเปิดรายงานในระบบ GUI
-      });
-
-      return config;
+      // implement node event listeners here
     },
   },
 });
