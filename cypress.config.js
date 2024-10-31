d14db73434f9dd9433440d7274a5c5fd05881cf5
sarayut Duangarpai <sarayutd7@gmail.com>
2024-10-31 12:49:42 +0700

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("cypress");
-const dotenv = require('cypress-dotenv');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
@@ -21,8 +20,6 @@ module.exports = defineConfig({
   projectId: "hkh24s",
   e2e: {
     setupNodeEvents(on, config) {
-      // Load environment variables from .env file
-      config = dotenv(config);
 
       // กำหนดค่า mochawesome reporter โดยไม่ต้อง require Cypress
       require('cypress-mochawesome-reporter/plugin')(on);
