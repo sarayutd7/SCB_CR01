134d4f55be498b09aa873d4d8e5fb90ee4445656
sarayut Duangarpai <sarayutd7@gmail.com>
2024-11-01 11:02:09 +0700

    catchup files

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,28 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'mochawesome',
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/reports',
+    reportDir: 'cypress/reports',   // ที่เก็บรายงาน  
+    reportFilename: 'test-report',
     overwrite: true,
     html: true,
-    json: true
-  },
+    json: false,
+    timestamp: 'mmddyyyy_HHMMss',
+    charts: true,
+    code: false,
+    autoOpen: true,
+    quiet: false,
+    inline: true,
+    saveJson: true,
+    embeddedScreenshots: true
+  }, 
   projectId: "hkh24s",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
     },
   },
 });
